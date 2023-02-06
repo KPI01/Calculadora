401e04a6373dd3d309522d1da656a24add289596
Jorge Ramirez <jorgelurd11@outlook.com>
2023-02-06 19:54:48 +0100

    Avanzado el formulario

---
+++ calculos.php
@@ -0,0 +1,6 @@
+<?php
+	function calculos(numero1, numero2)
+	{ 
+		echo $numero1 + $numero2
+	}
+?>
--- index.php
+++ index.php
@@ -7,10 +7,29 @@
     <title>Mi Calculadora en PHP</title>
 </head>
 <body>
-    <div class="calculadora">
-        <form action="">
-            <input type="text">
-        </form>
-    </div>
+	<h1>Mi Calculadora en PHP</h1>	
+	<div class="calculadora">
+		<form action="calculos.php" method="post">
+			<input type="number" name="numero1" placeholder="Aquí va el primer operando">
+			<select>
+				<option value="+">+</option>
+				<option value="-">-</option>
+				<option value="x">x</option>
+				<option value="/">/</option>
+			</select>
+			<input type="number" name="numero2" placeholder="Aquí va el segundo operando">
+			<input type="submit">
+		</form>
+	</div>
+	<div class="resultado">
+		<h3>Resultado:</h3>
+		<?php
+			include 'calculos.php';
+			$numero1 = $_POST["numero1"];
+			$numero2 = $_POST["numero2"];
+
+
+		?>
+	</div>
 </body>
 </html>
