16e0ba517318b207115a7fe95e7a3c3f006a4f3d
Jorge Ramirez <jorgelurd11@outlook.com>
2023-02-06 21:46:05 +0100

    Terminada la calculadora, ya 100% funcional

--- calculos.php
+++ calculos.php
@@ -1,6 +1,26 @@
 <?php
-	function calculos(numero1, numero2)
-	{ 
-		echo $numero1 + $numero2
+function operacion($operando1, $operando2, $operador)
+{
+	if (isset($operando1, $operando2, $operador)) {
+		if (is_numeric($operando1) || is_numeric($operando2)) {
+			if ($operador == "+") {
+				return $operando1 + $operando2;
+			} elseif ($operador == "-") {
+				return $operando1 - $operando2;
+			} elseif ($operador == 'x') {
+				return $operando1 * $operando2;
+			} elseif ($operador == "/") {
+				return $operando1 / $operando2;
+			} else {
+				return "Operador invalido";
+			}
+		} else {
+			return "ERROR-> se necesitan solo números para operar";
+		}
+	}else
+	{
+		$operando1="";
+		$operando2="";
+		$operador="";
 	}
-?>
+}
--- index.php
+++ index.php
@@ -1,35 +1,40 @@
+<?php
+		include 'calculos.php';
+		$resultado="0";
+
+		if(isset($_GET['num1'],$_GET['num2'],$_GET['operador'])){
+			$resultado=operacion($_GET['num1'],$_GET['num2'],$_GET['operador']);
+		}else{
+			$resultado="0";
+		}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
     <title>Mi Calculadora en PHP</title>
 </head>
 <body>
-	<h1>Mi Calculadora en PHP</h1>	
+	<h1>Mi Calculadora en PHP</h1>
 	<div class="calculadora">
-		<form action="calculos.php" method="post">
-			<input type="number" name="numero1" placeholder="Aquí va el primer operando">
-			<select>
+		<form action="<?php $_SERVER["PHP_SELF"]?>" method="GET">
+			<input type="text" class="campo" name="num1" value="" placeholder="1er operando">
+			<select name="operador">
 				<option value="+">+</option>
 				<option value="-">-</option>
 				<option value="x">x</option>
 				<option value="/">/</option>
 			</select>
-			<input type="number" name="numero2" placeholder="Aquí va el segundo operando">
-			<input type="submit">
+			<input type="text" class="campo" name="num2" value="" placeholder="2do operando">
+			<input type="submit" value="Calcular operación">
 		</form>
-	</div>
-	<div class="resultado">
-		<h3>Resultado:</h3>
-		<?php
-			include 'calculos.php';
-			$numero1 = $_POST["numero1"];
-			$numero2 = $_POST["numero2"];
-
-
-		?>
+		<input type="text" name="resultado" value="<?= $resultado ?>"disabled>
+		
+		</input>
 	</div>
 </body>
 </html>
